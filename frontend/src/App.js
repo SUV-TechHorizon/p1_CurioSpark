4b762e0610d8841a0c5df391b1b54a25572e86a6
ybhvdongre <vaibhavdon2@gmail.com>
2025-02-16 19:34:20 +0530

    ++ initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Body from "./Components/Body/Body";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div>
+        <h1 align='center'>
+        SUV-TechHorizon
+        </h1>
+        <Header />
+        <Body />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
