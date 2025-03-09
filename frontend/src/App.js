373fdd14e992674f15ac87de5c8a8d5a9fd98a9c
supritSP <supritpaunikar@gmail.com>
2025-03-09 18:29:03 +0530

    ++added course page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./Components/LandingPage";
 import Body from "./Components/Body/Body";
 import Navbar from "./Components/Header/Navbar";
 import Footer from "./Components/Footer/Footer";
+import Courses from "./Components/pages/Courses";
 
 function App() {
   const [activeSection, setActiveSection] = useState("home");
@@ -20,6 +21,7 @@ function App() {
         <Route path="/blog" element={<Body activeSection='blog' />}></Route>
         <Route path="/testimonials" element={<Body activeSection='testimonials' />}></Route>
         <Route path="/contact" element={<Body activeSection='contact' />}></Route>
+        <Route path="/courses/content" element={<Courses />}></Route>
       </Routes>
       <Footer />
     </>
